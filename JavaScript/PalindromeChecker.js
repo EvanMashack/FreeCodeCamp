9e1706cdf97cd27bf953bdf94740b83f68388179
Evan Mashack <99921516+EvanMashack@users.noreply.github.com>
2023-02-13 09:49:56 -0600

    Update PalindromeChecker.js

--- JavaScript/PalindromeChecker.js
+++ JavaScript/PalindromeChecker.js
@@ -1,1 +1,12 @@
+function palindrome(str) {
+    // Removing all non-alphanumeric characters and normalizing the case;
+    const regex = /[^A-Za-z0-9]/g
+    let scrubbedStr = str.replace(regex, "").toLowerCase(); 
 
+    // reversing the string for palindrome check
+    let reversedStr = scrubbedStr.split("").reverse().join(""); 
+  
+    return (scrubbedStr == reversedStr);
+  }
+  
+  console.log(palindrome("A man, a plan, a canal. Panama"));
