ec4306a43b213a49595efe099e1b4b252b4388c6
Evan Mashack <99921516+EvanMashack@users.noreply.github.com>
2023-02-28 12:08:51 -0600

    Update TelephoneValidator.js

--- JavaScript/TelephoneValidator.js
+++ JavaScript/TelephoneValidator.js
@@ -1,3 +1,5 @@
+// Assignment: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/telephone-number-validator
+// Test Case at the bottom of TelephoneValidator.js
 function telephoneValidator(str) {   
   // The format is 1(570)778-1016 with optional characters of, leading 1, (), -, a single space.
   const telephoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;   
@@ -5,5 +7,6 @@ function telephoneValidator(str) {
   return telephoneRegex.test(str);
 }
 
-console.log(telephoneValidator("1 (555) 555-5555"));
+// Test Case
+// console.log(telephoneValidator("1 (555) 555-5555"));
  
